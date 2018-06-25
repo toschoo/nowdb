4fa3ef256e3e56c9302c64c697d22b4de9a37227
toschoo <tobias.schoofs@gmx.net>
2018-06-25 19:56:38 +0200

    [develop] more benchmarks

--- test/sql/loadbig.sql
+++ test/sql/loadbig.sql
@@ -1,5 +1,7 @@
 create scope test2 if not exists;
 use test2;
 create big  context ctx_big  if not exists;
+create big index idx_big_edgori on ctx_big (edge, origin) if not exists;
+-- create big index idx_big_edgdst on ctx_big (edge, destin) if not exists;
 load '/opt/dbs/csv/hmillion.csv' into ctx_big;
 -- select * from ctx_tiny;
