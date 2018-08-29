4ef921f7026e81e84ac57c75b8b0f25ebde5750a
Tobias Schoofs <tobias.schoofs@gmx.net>
2018-08-29 19:17:02 +0100

    [ifc] minor changes to python

--- pynow/now.py
+++ pynow/now.py
@@ -1,14 +1,42 @@
+# -------------------------------------------------------------------------
+#  Basic Python NOWDB Client Interface
+#
+#  -----------------------------------
+#  (c) Tobias Schoofs, 2018
+#  -----------------------------------
+#  
+#  This file is part of the NOWDB CLIENT Library.
+# 
+#  The NOWDB CLIENT Library is free software; you can redistribute it
+#  and/or modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+# 
+#  The NOWDB CLIENT Library
+#  is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+# 
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with the NOWDB CLIENT Library; if not, see
+#  <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------
+
 from ctypes import *
 
+# ---- load libraries -----------------------------------------------------
 libc = cdll.LoadLibrary("libc.so.6")
 now = cdll.LoadLibrary("libnowdbclient.so")
 
+# ---- result types -------------------------------------------------------
 NOTHING = 0
 STATUS = 0x21
 REPORT = 0x22
 ROW = 0x23
 CURSOR = 0x24
 
+# ---- value types --------------------------------------------------------
 TEXT = 1
 DATE = 2
 TIME = 3
@@ -16,8 +44,10 @@ FLOAT = 4
 INT = 5
 UINT = 6
 
+# ---- C functions --------------------------------------------------------
 _explainError = now.nowdb_err_explain
 _explainError.restype = c_char_p
+_explainError.argtypes = [c_long]
 
 _connect = now.nowdb_connect
 _connect.restype = c_long
@@ -25,6 +55,7 @@ _connect.argtypes = [c_void_p, c_char_p, c_short, c_char_p, c_char_p, c_long]
 
 _closeCon = now.nowdb_connection_close
 _closeCon.restype = c_long
+_closeCon.argtypes = [c_void_p]
 
 _exec = now.nowdb_exec_statement
 _exec.restype = c_long
@@ -40,12 +71,15 @@ _rType.argtypes = [c_void_p]
 
 _rStatus = now.nowdb_result_status
 _rStatus.restype = c_long
+_rStatus.argtypes = [c_void_p]
 
 _rErrCode = now.nowdb_result_errcode
 _rErrCode.restype = c_long
+_rErrCode.argtypes = [c_void_p]
 
 _rDetails = now.nowdb_result_details
 _rDetails.restype = c_char_p
+_rDetails.argtypes = [c_void_p]
 
 _rRow = now.nowdb_cursor_row
 _rRow.restype = c_void_p
@@ -71,6 +105,7 @@ _rEof = now.nowdb_cursor_eof
 _rEof.restype = c_long
 _rEof.argtypes = [c_void_p]
 
+# ---- create a connection
 def connect(addr, port, usr, pwd):
     con = c_void_p()
     x = _connect(byref(con), c_char_p(addr), c_short(port), c_char_p(usr), c_char_p(pwd), c_long(0))
@@ -79,6 +114,7 @@ def connect(addr, port, usr, pwd):
     else:
         return (x,None)
 
+# ---- a connection
 class Connection:
     def __init__(self, con):
         self.con = con
@@ -91,6 +127,7 @@ class Connection:
         x = _exec(self.con, c_char_p(stmt), byref(r))
         return Result(r)
 
+# ---- result
 class Result:
     def __init__(self, r):
         self.r = r
