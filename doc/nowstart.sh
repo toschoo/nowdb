ac4be98ed69074f744face3915fb2be76b4073bd
Tobias Schoofs <tobias.schoofs@gmx.net>
2018-10-23 13:25:27 +0100

    [develop] docker stuff added to doc

--- doc/nowstart.sh
+++ doc/nowstart.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
 export PYTHONPATH=/usr/local/pynow:$PYTHONPATH
 
-nowdbd -b /dbs -y 2>log/nowdb.log
+nowdbd -b /opt/dbs -y 2>>/opt/log/nowdb.log
--- doc/start.sh
+++ doc/start.sh
@@ -1,1 +1,5 @@
-docker run --rm -p 55505:55505 -v /opt/dbs:/dbs -d 068db6b360fd /bin/bash -c "./nowstart.sh"
+docker run --rm -d                                 \
+	   -p 55505:55505                          \
+	   -v /opt:/opt                            \
+	   --name nowdbcontainer                   \
+	   nowdbdocker /bin/bash -c "./nowstart.sh"
