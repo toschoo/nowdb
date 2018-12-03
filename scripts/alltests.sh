5d7b6653ece171b3fecf9b6fb6da44db0a0805be
Tobias Schoofs <tobias.schoofs@gmx.net>
2018-12-03 13:50:05 +0000

    [vertex] refectoring fetch

--- scripts/alltests.sh
+++ scripts/alltests.sh
@@ -1,11 +1,13 @@
 echo "RUNNING TEST BATTERY"
 
+echo "RUNNING SMOKE TESTS IN C"
 scripts/smoke.sh
 if [ $? -ne 0 ]
 then
 	exit 1
 fi
 
+echo "RUNNING CLIENT TESTS"
 scripts/client.sh
 if [ $? -ne 0 ]
 then
@@ -14,6 +16,7 @@ fi
 
 sleep 1
 
+echo "RUNNING VWHERE TESTS"
 echo "RUNNING VWHERE TESTS" > log/where.log
 scripts/vwhere.sh 10 >>log/where.log 2>&1
 if [ $? -ne 0 ]
@@ -21,7 +24,9 @@ then
 	echo "FAILED: vwhere failed"
 	exit 1
 fi
+echo "PASSED"
 
+echo "RUNNING SMOKE TESTS IN PYTHON"
 scripts/pysmoke.sh
 if [ $? -ne 0 ]
 then
@@ -30,6 +35,7 @@ fi
 
 sleep 1
 
+echo "RUNNING EWHERE TESTS"
 echo "RUNNING EWHERE TESTS" >> log/where.log
 scripts/ewhere.sh 10 1>&2 >>log/where.log 2>&1
 if [ $? -ne 0 ]
@@ -37,3 +43,4 @@ then
 	echo "FAILED: ewhere failed"
 	exit 1
 fi
+echo "PASSED"
