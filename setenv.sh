6d19ab72de3cb53cc1e5550437a7e5ac0dd523dd
toschoo <tobias.schoofs@gmx.net>
2019-04-12 15:37:45 +0200

    [todo-22] added more to LUA_PATH

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,5 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib:../lib:/usr/local/lib
 export PATH=$PATH:./bin:./todo/bin
 export PYTHONPATH=$PYTHONATH:./pynow:./test/server
-export LUA_PATH="$LUA_PATH;./lua/?.lua;./test/server/?.lua;./test/luasmoke/?.lua;/home/pj/luanow/?.lua"
+export LUA_PATH="$LUA_PATH;./lua/?.lua;./test/server/?.lua;./test/luasmoke/?.lua;/home/pj/luanow/?.lua;../luanow/?.lua"
 export LUA_CPATH="$LUA_CPATH;./lua/?.so;./test/server/?.so"
