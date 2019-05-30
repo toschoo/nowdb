f43c8cd2edb28b9826926dcfebc58643aa12edc2
toschoo <tobias.schoofs@gmx.net>
2019-05-30 18:44:01 +0200

    [todo-22] docker

--- doc/nowstart.sh
+++ doc/nowstart.sh
@@ -1,4 +1,8 @@
 export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
 export PYTHONPATH=/usr/local/pynow:$PYTHONPATH
+export LUA_PATH="./lua/?.lua;/opt/lua/?.lua;"
+export LUA_CPATH="$LUA_CPATH;./lua/?.so;/opt/lua/?.so"
+export NOWDB_LUA_PATH="*:/opt/lua"
+export NOWDB_HOME="/usr/local"
 
-nowdbd -b /opt/dbs -y 2>>/opt/log/nowdb.log
+nowdbd -p 50677 -b /opt/dbs -l >/opt/log/nowdb.log 2>&1
