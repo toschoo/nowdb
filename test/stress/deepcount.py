f29d0623ca6484cb785ce273054cef4d4ac78b61
toschoo <tobias.schoofs@gmx.net>
2020-02-16 13:55:13 +0100

    [master] minor change in deepcount

--- test/stress/deepcount.py
+++ test/stress/deepcount.py
@@ -14,6 +14,7 @@ class QThread(threading.Thread):
   def run(self):
     try:
       with Connection("127.0.0.1", "55505", None, None) as con:
+      # with Connection("127.0.0.1", "50677", None, None) as con:
         with con.execute("use retail") as r:
           if not r.ok():
              print("ERROR: %d on use: %s" % (r.code(), r.details()))
